4cf8dab26594195c2e1fb7985aaa199fb2e656dc
Emre Zorlu <ahmet@emrezorlu.com>
2022-08-15 20:55:01 +0300

    spring config file

---
+++ src/main/java/com/emrezorlu/app/demo/airwayflightplan/AirwayFlightPlanApplication.java
@@ -0,0 +1,13 @@
+package com.emrezorlu.app.demo.airwayflightplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirwayFlightPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirwayFlightPlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emrezorlu/app/demo/airwayflightplan/AirwayFlightPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emrezorlu.app.demo.airwayflightplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirwayFlightPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
